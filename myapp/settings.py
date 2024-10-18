ef1bd8054d816b21994cb0f8b43fce388ee6e807
Chef <cheftech@patraldo.com>
2024-10-17 21:33:05 -0600

    -

--- myapp/settings.py
+++ myapp/settings.py
@@ -92,8 +92,8 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "/data/db.sqlite3", #Production -- comment/uncomment
-        #"NAME": BASE_DIR / "db.sqlite3",  #local development -- comment/uncomment
+        #"NAME": BASE_DIR / "/data/db.sqlite3", #Production -- comment/uncomment
+        "NAME": BASE_DIR / "db.sqlite3",  #local development -- comment/uncomment
     }
 }
 
